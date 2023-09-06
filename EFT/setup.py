11a9d20df24298741a447498ad653099e4a5f208
PossiblePanda <85448494+PossiblePanda@users.noreply.github.com>
2023-09-06 16:24:26 -0400

    Added setup.py

---
+++ EFT/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'A package to easily create GUI themes.'
+LONG_DESCRIPTION = 'A package that allows easy creation of GUI themes for your application.'
+
+# Setting up
+setup(
+    name="possiblepanda",
+    version=VERSION,
+    author="NeuralNine (Florian Dedov)",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'theme', 'gui', 'gui theme', 'python theme', 'gui application'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers, ",
+        "Intended Audience :: End Users/Desktop",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
